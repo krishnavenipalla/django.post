eed3c709aefb43de75496ae19b701f5638a09302
palla krishnaveni <krishnavenipalla2003@gmail.com>
2023-07-19 11:35:23 +0530

    fetched data free db

---
+++ instagram/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from.models import Post
+
+# Create your views here.
+
+def index(request):
+
+    post = Post.objects.all()
+
+    print(post)
+
+    return render(request, 'index.html',context={'posts':post})
+def about(request):
+    return render(request, 'about.html')
+
